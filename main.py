952bef59227efe79dffbdf05cb68799860cd4f63
dev-diegov <diegoidv.us@gmail.com>
2023-08-14 12:13:01 -0400

    KEYS logic

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import pygame
 
-#starts pygame
+# starts pygame
 pygame.init()
 
-#screen size
+# screen size
 screen = pygame.display.set_mode((1280, 720)) #800 width, 600 height original
 
-#títle and icon
+# títle and icon
 pygame.display.set_caption("Zombie Invasion")
 icon = pygame.image.load("skullLogo.png")
 pygame.display.set_icon(icon)
 
-#player
+# player
 img_player = pygame.image.load("player.png")
 player_pos_x = 10
 player_pos_y = 328 # screen height / 2 - 32 (image's size 64px/2)
+player_movement = 0
 
-#var to check if screen's player is opened/closed
+# var to check if screen's player is opened/closed
 is_executed = True
 
-def player():
+def player(x, y):
     screen.blit(img_player, (player_pos_x, player_pos_y))
 
 while is_executed:
 
-    #background screen's color
+    # background screen's color
     screen.fill((2, 191, 252))
 
     # check if user closes the window
@@ -33,7 +34,21 @@ while is_executed:
         if event.type == pygame.QUIT:
             is_executed = False
 
-    #we set player's icon on screen
-    player()
+        # check if player keep pressed KEYs left, right and his movement speed 0.5
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_movement = -0.5
+            if event.key == pygame.K_RIGHT:
+                player_movement = 0.5
+
+        # check if player is NOT pressing any key then reset player movement's to 0
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_movement = 0
+
+    # set player position depending on player_movement's var value
+    player_pos_y += player_movement
+    # we set player's skin on screen
+    player(player_pos_x, player_pos_y)
 
     pygame.display.update()
